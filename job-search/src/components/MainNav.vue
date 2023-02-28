9ee8656c48330f35768d7750e6d85fced79fcfc3
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2023-02-28 16:49:01 +0200

    first easy tests

---
+++ job-search/src/components/MainNav.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { computed, reactive } from "vue";
+const company = computed(() => "Dima Careers");
+const author = reactive({
+  firstName: "Dima",
+  lastName: "Pov",
+});
+</script>
+
+<template>
+  <header class="brand-green-1 w-full text-3xl">
+    <div class="fixed top-0 left-0 h-16 w-full bg-white">
+      <div
+        class="flex-no-wrap flex h-full border-b border-solid border-brand-gray-1 px-8"
+      >
+        <a class="flex h-full items-center text-xl" href="/">{{ company }}</a>
+        <h2 class="ml-8 flex h-full items-center">
+          Developed by {{ author.firstName }}
+        </h2>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ job-search/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  theme: {
+    extend: {
+      FontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "brand-gray-1": "#dadce0",
+        "brand-blue-1": "#1967d2",
+        "brand-green-1": "#137333",
+      },
+    },
+  },
+  plugins: [],
+};
