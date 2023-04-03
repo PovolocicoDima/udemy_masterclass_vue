be236c162e107de3c46e9b843c050ebdd7e03122
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2023-04-03 13:10:35 +0300

    headerContainer added plus test

--- job-search/tests/unit/components/Shared/CollapsebleAccordeon.test.js
+++ job-search/tests/unit/components/Shared/CollapsebleAccordeon.test.js
@@ -1,53 +1,53 @@
 import { render, screen } from "@testing-library/vue";
 import userEvent from "@testing-library/user-event";
-import CollapsebleAccordeon from "@/components/Shared/CollapsebleAccordeon.vue";
+import CollapsebleAccordeon from "@/components/Shared/CollapsibleAccordion.vue";
 
 describe("CollapsebleAccordeon.vue", () => {
-  const renderCollapsebleAccordeon = (props, slots) => {
-    render(CollapsebleAccordeon, {
-      global: {
-        stubs: {
-          FontAwesomeIcon: true,
-        },
-      },
-      props,
-      slots,
-    });
-  };
+    const renderCollapsebleAccordeon = (props, slots) => {
+        render(CollapsebleAccordeon, {
+            global: {
+                stubs: {
+                    FontAwesomeIcon: true,
+                },
+            },
+            props,
+            slots,
+        });
+    };
 
-  it("renders child content when clicked", async () => {
-    renderCollapsebleAccordeon(
-      {
-        header: "My category",
-        childContent: "My nested content",
-      },
-      {
-        default: `
+    it("renders child content when clicked", async () => {
+        renderCollapsebleAccordeon(
+            {
+                header: "My category",
+                childContent: "My nested content",
+            },
+            {
+                default: `
           <template #default>
             <p>My nested content</p>
           </template>
         `,
-      }
-    );
+            }
+        );
 
-    expect(screen.queryByText("My nested content")).not.toBeInTheDocument();
-    const button = screen.getByRole("button", { name: /my category/i });
-    await userEvent.click(button);
-    expect(screen.getByText("My nested content")).toBeInTheDocument();
-  });
-
-  describe("when parent does NOT provide custom child content", () => {
-    it("renders default content", async () => {
-      renderCollapsebleAccordeon({
-        header: "My category",
-        childContent: "My nested content",
-      });
-
-      const button = screen.getByRole("button", { name: /my category/i });
-      await userEvent.click(button);
-      expect(
-        screen.getByText("Whooops, somebody forget to populate me!")
-      ).toBeInTheDocument();
+        expect(screen.queryByText("My nested content")).not.toBeInTheDocument();
+        const button = screen.getByRole("button", { name: /my category/i });
+        await userEvent.click(button);
+        expect(screen.getByText("My nested content")).toBeInTheDocument();
     });
-  });
+
+    describe("when parent does NOT provide custom child content", () => {
+        it("renders default content", async () => {
+            renderCollapsebleAccordeon({
+                header: "My category",
+                childContent: "My nested content",
+            });
+
+            const button = screen.getByRole("button", { name: /my category/i });
+            await userEvent.click(button);
+            expect(
+                screen.getByText("Whoops, somebody forgot to populate me!")
+            ).toBeInTheDocument();
+        });
+    });
 });
---
+++ job-search/tests/unit/components/Shared/HeaderContainer.test.js
@@ -0,0 +1,24 @@
+import { screen, render } from "@testing-library/vue";
+import HeaderContainer from "@/components/Shared/HeaderContainer.vue";
+
+describe("HeaderContainer.vue", () => {
+    it("allows parent component to provide title content", () => {
+        render(HeaderContainer, {
+            slots: {
+                title: "<p>Some title</p>",
+            },
+        });
+
+        expect(screen.getByText("Some title")).toBeInTheDocument();
+    });
+
+    it("allows parent component to provide subtitle content", () => {
+        render(HeaderContainer, {
+            slots: {
+                subtitle: "<h3>Some subtitle</h3>",
+            },
+        });
+
+        expect(screen.getByText("Some subtitle")).toBeInTheDocument();
+    });
+});
