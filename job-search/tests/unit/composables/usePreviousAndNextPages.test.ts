2e19d93300b5e03b0b64d7eb89fd09cb4b591829
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2023-04-10 17:10:43 +0300

    added test and ts version for prevNextPage component

--- job-search/tests/unit/composables/usePreviousAndNextPages.test.ts
+++ job-search/tests/unit/composables/usePreviousAndNextPages.test.ts
@@ -1,33 +1,34 @@
 import usePreviousAndNextPages from "@/composables/usePreviousAndNextPage";
+import { ref } from "vue";
 
 describe("usePreviousAndNextPages", () => {
   it("calculates page before current one", () => {
-    const currentPage = { value: 8 };
-    const maxPage = { value: 10 };
+    const currentPage = ref(8);
+    const maxPage = ref(10);
     const { previousPage } = usePreviousAndNextPages(currentPage, maxPage);
     expect(previousPage.value).toBe(7);
   });
 
   describe("when current page is the first page", () => {
     it("does NOT provide previous page", () => {
-      const currentPage = { value: 1 };
-      const maxPage = { value: 1 };
+      const currentPage = ref(1);
+      const maxPage = ref(1);
       const { previousPage } = usePreviousAndNextPages(currentPage, maxPage);
       expect(previousPage.value).toBeUndefined();
     });
   });
 
   it("calculates page after current one", () => {
-    const currentPage = { value: 2 };
-    const maxPage = { value: 10 };
+    const currentPage = ref(2);
+    const maxPage = ref(10);
     const { nextPage } = usePreviousAndNextPages(currentPage, maxPage);
     expect(nextPage.value).toBe(3);
   });
 
   describe("when current page is the last page", () => {
     it("does NOT provide next page", () => {
-      const currentPage = { value: 9 };
-      const maxPage = { value: 9 };
+      const currentPage = ref(9);
+      const maxPage = ref(9);
       const { nextPage } = usePreviousAndNextPages(currentPage, maxPage);
       expect(nextPage.value).toBeUndefined();
     });
